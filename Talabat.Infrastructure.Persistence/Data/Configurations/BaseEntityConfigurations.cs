338e09ecbabfaf90f3f5351dc7adf351c9fb9164
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-08-20 05:31:45 +0300

    Add entity configuration classes and update project structure
    - Updated `GlobalUsings.cs` to include `Talabat.Domain.Common`.
    - Removed folder reference for `Data\Configurations\` in `.csproj`.
    - Added `BaseAuditableEntityConfigurations` for auditable entities.
    - Added `BaseEntityConfigurations` to configure entity `Id` properties.

---
+++ Talabat.Infrastructure.Persistence/Data/Configurations/BaseEntityConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Talabat.Infrastructure.Persistence.Data.Configurations
+{
+    public class BaseEntityConfigurations<TEntity,Tkey> : IEntityTypeConfiguration<TEntity>
+        where TEntity: BaseEntity<Tkey>
+        where Tkey : IEquatable<Tkey>
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+           builder.Property(e => e.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
