cb9e46ce25b843b847a7f1ca8f8ef0d49e89640b
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-07 22:51:54 +0300

    Add Specifications folder and ISpecifications interface
    Updated `Talabat.Domain.csproj` to include a new folder
    item group for "Specifications". Added a new generic
    interface `ISpecifications` in `ISpecifications.cs`
    to define criteria for filtering entities using
    `Expression<Predicate<TEntity>>`.

---
+++ Talabat.Domain/Contracts/ISpecifications.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Talabat.Domain.Contracts
+{
+    public interface ISpecifications <TEntity ,TKey>
+        where TEntity: BaseEntity<TKey>
+        where TKey: IEquatable<TKey>
+    {
+        public Expression<Predicate<TEntity>> Criteria { get; set; }
+    }
+}
+ 
