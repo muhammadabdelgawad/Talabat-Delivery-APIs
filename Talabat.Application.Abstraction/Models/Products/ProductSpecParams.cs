e199c5661fdf4b6d5a877700e061eb4394b5cbb5
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-10 06:11:32 +0300

    Add pagination support to ProductSpecParams
    Updated the ProductSpecParams record to include
    PageIndex and PageSize properties. The PageSize
    property enforces a maximum limit of 50, enhancing
    pagination functionality for product specifications.

--- Talabat.Application.Abstraction/Models/Products/ProductSpecParams.cs
+++ Talabat.Application.Abstraction/Models/Products/ProductSpecParams.cs
@@ -11,5 +11,13 @@ namespace Talabat.Application.Abstraction.Models.Products
         public string?  Sort { get; set; }
         public int? BrandId { get; set; }
         public int? CategoryId { get; set; }
+        public int PageIndex { get; set; }
+        private const int maxPageSize = 50;
+        private int pageSize;
+        public int PageSize
+        {
+            get => pageSize;
+            set { PageSize = value > maxPageSize ? maxPageSize : value; }
+        }
     }
 }
