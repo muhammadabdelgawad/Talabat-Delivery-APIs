befb10727c1db6a7fe9e3f51379923de82251862
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-23 15:08:35 +0300

    Add BasketDto and BasketItemDto records
    Introduced `BasketDto` and `BasketItemDto` records in the
    `Talabat.Application.Abstraction.Models.Basket` namespace.
    
    - `BasketDto` includes `Id` and a collection of `BasketItemDto`.
    - `BasketItemDto` includes properties such as `Id`, `ProductName`,
      `PictureUrl`, `Price`, `Quantity`, `Brand`, and `Category`.

---
+++ Talabat.Application.Abstraction/Models/Basket/BasketDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Talabat.Application.Abstraction.Models.Basket
+{
+    public record BasketDto
+    {
+        public required int Id { get; set; }
+        public required IEnumerable<BasketItemDto> Items { get; set; }
+    }
+}
---
+++ Talabat.Application.Abstraction/Models/Basket/BasketItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Talabat.Application.Abstraction.Models.Basket
+{
+    public record BasketItemDto
+    {
+        public int Id { get; set; }
+        public required string ProductName { get; set; }
+        public string? PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string? Brand { get; set; }
+        public string? Category { get; set; }
+    }
+}
+  
