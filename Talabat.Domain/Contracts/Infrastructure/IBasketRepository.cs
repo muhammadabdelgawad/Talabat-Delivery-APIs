150dda4984618b7c861ff81505952c5b8c4a5351
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-23 11:34:16 +0300

    Add Redis integration and basket repository support
    Integrated Redis for basket storage by adding `IBasketRepository` and its implementation in `BasketRepository`. Updated `appsettings.json` with Redis connection string. Registered infrastructure services in `Program.cs` and added `DependencyInjection` for Redis configuration. Updated `Talabat.Infrastructure.csproj` with necessary dependencies and project references. Added global usings and made minor namespace adjustments.

---
+++ Talabat.Domain/Contracts/Infrastructure/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Talabat.Domain.Contracts.Infrastructure
+{
+    public interface IBasketRepository
+    {
+        Task<Basket?> GetAsync(string id);
+        Task<Basket?> UpdateAsync(Basket basket, TimeSpan timeToLive);
+        Task DeleteAsync(string id);
+
+    }
+}
