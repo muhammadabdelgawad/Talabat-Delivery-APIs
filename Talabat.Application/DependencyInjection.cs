5d90720292831ac46365d96b354dc8af8453f06d
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-23 23:47:20 +0300

    Add support for BasketService with DI and lazy loading
    Updated DependencyInjection.cs to register a factory for
    BasketService using scoped dependencies (IMapper,
    IConfiguration, IBasketRepository). Enhanced ServiceManager
    to include lazy initialization for BasketService and added
    support for IConfiguration. Introduced new using directives
    to ensure required dependencies are available.

--- Talabat.Application/DependencyInjection.cs
+++ Talabat.Application/DependencyInjection.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Talabat.Application.Abstraction.Services;
+using Talabat.Application.Abstraction.Services.Basket;
 using Talabat.Application.Maping;
 using Talabat.Application.Services;
+using Talabat.Domain.Contracts.Infrastructure;
+using Talabat.Infrastructure.Persistence.Repositories.Baskets;
 namespace Talabat.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            // Fix: Use the correct overload of AddAutoMapper that accepts assembly types
+            
             services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
             services.AddScoped(typeof(IServiceManager), typeof(ServiceManager));
             services.AddScoped<ProductPictureUlrResolver>();
-            return services;
+            services.AddScoped(typeof(Func<IBasketService>), (serviceProvider) =>
+            {
+                var _mapper = serviceProvider.GetRequiredService<IMapper>();
+                var _configuration = serviceProvider.GetRequiredService<IConfiguration>();
+                var _basketRepository = serviceProvider.GetRequiredService<IBasketRepository>();
+                return () => new BasketService(_basketRepository,_mapper, _configuration);
+            });
+                
+                return services;
         }
     }
 }
--- Talabat.Application/Services/ServiceManager.cs
+++ Talabat.Application/Services/ServiceManager.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
 using Talabat.Application.Abstraction.Services;
+using Talabat.Application.Abstraction.Services.Basket;
 using Talabat.Application.Abstraction.Services.Products;
 using Talabat.Application.Services.Products;
 using Talabat.Domain.Contracts.Presistence;
+using Talabat.Infrastructure.Persistence.Repositories.Baskets;
 namespace Talabat.Application.Services
 {
     public class ServiceManager : IServiceManager
@@ -10,12 +13,20 @@ namespace Talabat.Application.Services
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly Lazy<IProductService> _productService;
-        public ServiceManager(IUnitOfWork unitOfWork , IMapper mapper)
+        private readonly Lazy<IBasketService> _basketService;
+        private readonly IConfiguration _configuration;
+        public ServiceManager(IUnitOfWork unitOfWork , IMapper mapper,
+            IConfiguration configuration,Func<IBasketService> basketServiceFactory)
         {
-            _unitOfWork=unitOfWork;
+            _configuration=configuration;
+            _unitOfWork =unitOfWork;
             _mapper=mapper;
             _productService=new Lazy<IProductService>(()=>new ProductService(_unitOfWork,_mapper));
+            _basketService = new Lazy<IBasketService>(basketServiceFactory);
         }
         public IProductService ProductService  =>_productService.Value;
+
+        public IBasketService BasketService => throw new NotImplementedException();
     }
 }
+ 
